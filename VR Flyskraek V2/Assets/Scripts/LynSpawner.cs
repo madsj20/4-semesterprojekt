9ff5c16e6ad002c6f24efda2e1d1506a5b1aa6ac
mads juhl <majuh18@student.sdu.dk>
2023-04-23 20:11:20 +0200

    hj

---
+++ VR Flyskraek V2/Assets/Scripts/LynSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LynSpawner : MonoBehaviour
+{
+
+
+    public GameObject objectToSpawn;
+    public int numberOfObjects;
+    public float spawnRadius;
+    public Transform spawnPoint;
+
+    public void Spawn()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            Vector3 randomPos = Random.insideUnitSphere * spawnRadius;
+            Instantiate(objectToSpawn, spawnPoint.position + randomPos, Quaternion.identity);
+        }
+    }
+
+
+}
