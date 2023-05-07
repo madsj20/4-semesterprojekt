1c0c12fb2b8ca12be726aed4a32646a3563c72e4
Mads Jensen <madsj20@student.sdu.dk>
2023-05-07 18:13:39 +0200

    Added shake audio

--- VR Flyskraek V2/Assets/Scripts/StormScene.cs
+++ VR Flyskraek V2/Assets/Scripts/StormScene.cs
@@ -9,6 +9,7 @@ public class StormScene : MonoBehaviour
     private bool passiveShaking;
     private bool forceStopShaking = false;
     public PlayerController playerController;
+    public AudioSource shakeSound;
 
     // Start is called before the first frame update
     void Start()
@@ -35,12 +36,17 @@ public class StormScene : MonoBehaviour
         float magnitude;
         //shakeDelay determines the delay between shakes
         float shakeDelay;
+        //ShakePitch changes the pitch of the shake audio to a random value
+        float shakePitch;
 
         //Randomizing screenshake
         shakeTime = Random.Range(0.15f, 0.03f);
         magnitude = Random.Range(-0.03f, -0.01f);
         shakeDelay = Random.Range(0.2f, 2.5f);
+        shakePitch = Random.Range(0.5f, 1.1f);
 
+        shakeSound.pitch = shakePitch;
+        shakeSound.Play();
         playerController.InitScreenShake(magnitude, shakeTime);
         //shakeTime has to be added and multiplied by 2, to give the animation time to finish, bacause shakeTime is used 2 times in the original function in the PlayerController script
         yield return new WaitForSeconds(shakeDelay+(shakeTime*2));
@@ -75,9 +81,8 @@ public class StormScene : MonoBehaviour
 
         //Begin Turbulens
 
-
     }
-
+    
     public void initiateStorm()
     {
         StartCoroutine(Storm());
